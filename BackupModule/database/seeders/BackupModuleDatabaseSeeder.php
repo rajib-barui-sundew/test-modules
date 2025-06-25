5d5513a5a7ae2e979bf549924ff4227975b05718
Rajib Barui <rajib.barui@sundewsolutions.com>
2025-06-25 17:10:23 +0530

    first commit

---
+++ BackupModule/database/seeders/BackupModuleDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\BackupModule\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class BackupModuleDatabaseSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    // $this->call([]);
+  }
+}
---
+++ BackupModule/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\BackupModule\Http\Controllers\BackupController;
+
+Route::get('/db-backup', [BackupController::class, 'index'])->name('db-backup');
