e0ff0719e33c370e398279c25fa1d3f59fd53f7a
Rajib Barui <rajib.barui@sundewsolutions.com>
2025-07-01 17:11:20 +0530

    rrr

--- BackupModule/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\BackupModule\Http\Controllers\BackupController;
-
-Route::get('/db-backup', [BackupController::class, 'index'])->name('db-backup');
